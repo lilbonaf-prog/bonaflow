9739529f4329b8e769b3ca94251bc39afbd968c4
lilbonaf-prog <lilbonaf@gmail.com>
2026-09-06 23:30:53 +0000

    Convert BonaFlow to an installable PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,49 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png'],
+      manifest: {
+        name: 'BonaFlow',
+        short_name: 'BonaFlow',
+        description: 'Run your business. Know your numbers.',
+        theme_color: '#101B33',
+        background_color: '#F6F7F9',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
 })
